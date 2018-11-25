8493ef3b979218635513e5312eadf172cc2b51cf
Athiya Deviyani <athiyadee@gmail.com>
2018-11-25 01:17:52 +0000

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,146 @@
+from tkinter import *
+from tkinter import messagebox
+import boto3
+import json 
+import csv
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+window = Tk()
+window.title("StudentReacts")
+
+window.geometry('200x200')
+
+def create_report():
+    inf2a = []
+    inf2ccs = []
+    inf2cse = []
+    dmmr = []
+
+    with open('feedback.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                if (row[1] == 'INF2A'):
+                    inf2a.append(row[2])
+                elif (row[1] == 'INF2C-CS'):
+                    inf2ccs.append(row[2])
+                elif (row[1] == 'INF2C-SE'):
+                    inf2cse.append(row[2])
+                elif (row[1] == 'DMMR'):
+                    dmmr.append(row[2])
+
+        course_arrays = [inf2a, inf2ccs, inf2cse, dmmr]
+
+        for course in course_arrays:
+            mixedcount = 0
+            positivecount = 0
+            negativecount = 0
+            neutralcount = 0
+            total = 0
+            for feedback in course:
+                total += 1
+                comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+                result = json.dumps(comprehend.detect_sentiment(Text=feedback, LanguageCode='en'), sort_keys=True, indent=4)
+                stuff = json.loads(result)
+
+                for k in stuff.items():
+                    if 'Sentiment' in k:
+                        result = k[1].lower()
+                        if (result == "positive"):
+                            positivecount += 1
+                        if (result == "negative"):
+                            negativecount += 1
+                        if (result == "neutral"):
+                            neutralcount += 1
+                        if (result == "mixed"):
+                            mixedcount += 1
+
+            #print("COURSE NAME: " + row[1])
+            print("")
+            if course == inf2a:
+                print("COURSE NAME: INF2A")
+            elif course == inf2ccs:
+                print("COURSE NAME: INF2C-CS")
+            elif course == inf2cse:
+                print("COURSE NAME: INF2C-SE")
+            elif course == dmmr:
+                print("COURSE NAME: DMMR")
+
+            print("POSITIVE: " + "%.2f" % (positivecount * 100/ total) + "%")
+            print("NEGATIVE: " + "%.2f" % (negativecount * 100/ total) + "%")
+            print("MIXED: " + "%.2f" % (mixedcount * 100/ total) + "%")
+            print("NEUTRAL: " + "%.2f" % (neutralcount * 100/ total) + "%")
+            print("")
+            print("#######################################")
+            print("")
+
+
+    # for course in course_arrays:
+    
+            # Data to plot
+            title = ""
+            if course == inf2a:
+                title = "INF2A"
+            elif course == inf2ccs:
+                title = "INF2C-CS"
+            elif course == inf2cse:
+                title = "INF2C-SE"
+            elif course == dmmr:
+                title = "DMMR"
+
+            plt.title(title)
+            labels = 'Positive', 'Negative', 'Mixed', 'Neutral'
+            sizes = [positivecount * 100/ total, negativecount * 100/ total, mixedcount * 100/ total, neutralcount * 100/ total]
+            colors = ['yellowgreen', 'lightcoral', 'lightskyblue', 'gold']
+            plt.pie(sizes)
+            patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
+            plt.legend(patches, labels, loc="best")
+            plt.axis('equal')
+            plt.tight_layout()
+            #plt.show()
+            plt.savefig(title + '_report.png')
+    
+    messagebox.showinfo('MESSAGE', 'Reports are successfully generated!')
+    exit()
+
+lbl = Label(window, text = "", font = ("Arial Bold",20))
+lbl.pack()
+
+btn = Button(window, text = "Generate Report", command = create_report, font = ("Arial",20))
+btn.pack()
+
+
+window.mainloop()
+
+
+#root.mainloop()
+        #if row > 0:
+            #text = (row[2])
+
+#            comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+            #text = "I am not sure about my feelings towards Calculus."
+ #           result = json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4)
+  #          stuff = json.loads(result)
+
+           # print(stuff)
+
+    #        result = ""
+   #         for k in stuff.items():
+     #           if 'Sentiment' in k:
+      #              result = k[1].lower()
+       #             print(k[1])
+
+        #    for j in stuff.items():
+         #       if 'SentimentScore' in j:
+          #          for i in (j[1]).keys():
+           #             if result.lower() in i.lower():
+            #                percentage = j[1][i] * 100    
+             #               print("%.2f" % percentage + "%")
+
