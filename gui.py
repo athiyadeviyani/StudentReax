8403ae32450c0c9298d11dd82d26a7aa719cbb93
Athiya Deviyani <athiyadee@gmail.com>
2018-11-25 05:58:40 +0000

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,11 +7,35 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from tkinter import filedialog as fd
+import os
+import PIL.Image 
+import PIL.ImageTk
 
 window = Tk()
-window.title("StudentReacts")
+window.title("Student | REAX")
 
-window.geometry('200x200')
+window.geometry('300x400')
+
+spc = Label(window, text = "", font = ("Arial Bold",20))
+spc.pack()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    
+    return os.path.join(base_path, relative_path)
+
+image = PIL.Image.open(resource_path("logo.png")).resize((200, 200), PIL.Image.ANTIALIAS)
+photo = PIL.ImageTk.PhotoImage(image)
+
+label = Label(image=photo)
+label.image = photo # keep a reference!
+label.pack()
 
 def create_report():
     inf2a = []
@@ -19,7 +43,9 @@ def create_report():
     inf2cse = []
     dmmr = []
 
+    #with open('feedback.csv') as csv_file:
     with open('feedback.csv') as csv_file:
+        
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
 
@@ -62,26 +88,6 @@ def create_report():
                         if (result == "mixed"):
                             mixedcount += 1
 
-            #print("COURSE NAME: " + row[1])
-            print("")
-            if course == inf2a:
-                print("COURSE NAME: INF2A")
-            elif course == inf2ccs:
-                print("COURSE NAME: INF2C-CS")
-            elif course == inf2cse:
-                print("COURSE NAME: INF2C-SE")
-            elif course == dmmr:
-                print("COURSE NAME: DMMR")
-
-            print("POSITIVE: " + "%.2f" % (positivecount * 100/ total) + "%")
-            print("NEGATIVE: " + "%.2f" % (negativecount * 100/ total) + "%")
-            print("MIXED: " + "%.2f" % (mixedcount * 100/ total) + "%")
-            print("NEUTRAL: " + "%.2f" % (neutralcount * 100/ total) + "%")
-            print("")
-            print("#######################################")
-            print("")
-
-
     # for course in course_arrays:
     
             # Data to plot
@@ -100,47 +106,109 @@ def create_report():
             sizes = [positivecount * 100/ total, negativecount * 100/ total, mixedcount * 100/ total, neutralcount * 100/ total]
             colors = ['yellowgreen', 'lightcoral', 'lightskyblue', 'gold']
             plt.pie(sizes)
-            patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
+            patches, texts = plt.pie(sizes, colors=colors, startangle=90)
             plt.legend(patches, labels, loc="best")
             plt.axis('equal')
             plt.tight_layout()
             #plt.show()
             plt.savefig(title + '_report.png')
     
+
+    
     messagebox.showinfo('MESSAGE', 'Reports are successfully generated!')
     exit()
+
+def view_statistics():
+    inf2a = []
+    inf2ccs = []
+    inf2cse = []
+    dmmr = []
+
+    #with open('feedback.csv') as csv_file:
+    with open('feedback.csv') as csv_file:
+        
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                if (row[1] == 'INF2A'):
+                    inf2a.append(row[2])
+                elif (row[1] == 'INF2C-CS'):
+                    inf2ccs.append(row[2])
+                elif (row[1] == 'INF2C-SE'):
+                    inf2cse.append(row[2])
+                elif (row[1] == 'DMMR'):
+                    dmmr.append(row[2])
+
+        course_arrays = [inf2a, inf2ccs, inf2cse, dmmr]
+        f = open('output.txt','w')
+
+        for course in course_arrays:
+            mixedcount = 0
+            positivecount = 0
+            negativecount = 0
+            neutralcount = 0
+            total = 0
+            for feedback in course:
+                total += 1
+                comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+                result = json.dumps(comprehend.detect_sentiment(Text=feedback, LanguageCode='en'), sort_keys=True, indent=4)
+                stuff = json.loads(result)
+
+                for k in stuff.items():
+                    if 'Sentiment' in k:
+                        result = k[1].lower()
+                        if (result == "positive"):
+                            positivecount += 1
+                        if (result == "negative"):
+                            negativecount += 1
+                        if (result == "neutral"):
+                            neutralcount += 1
+                        if (result == "mixed"):
+                            mixedcount += 1
+
+            #print("COURSE NAME: " + row[1])
+            print("",file=f)
+            if course == inf2a:
+                print("COURSE NAME: INF2A",file=f)
+            elif course == inf2ccs:
+                print("COURSE NAME: INF2C-CS", file=f)
+            elif course == inf2cse:
+                print("COURSE NAME: INF2C-SE", file=f)
+            elif course == dmmr:
+                print("COURSE NAME: DMMR", file=f)
+
+            print("POSITIVE: " + "%.2f" % (positivecount * 100/ total) + "%", file=f)
+            print("NEGATIVE: " + "%.2f" % (negativecount * 100/ total) + "%", file=f)
+            print("MIXED: " + "%.2f" % (mixedcount * 100/ total) + "%", file=f)
+            print("NEUTRAL: " + "%.2f" % (neutralcount * 100/ total) + "%", file=f)
+            print("",file=f)
+            print("#######################################",file=f)
+            print("",file=f)
+    
+    messagebox.showinfo('MESSAGE', 'Complete statistics are successfully printed to output.txt!')
+    exit()
+
+def view_all():
+    os.system("open " + str(resource_path("feedback.csv")))
 
 lbl = Label(window, text = "", font = ("Arial Bold",20))
 lbl.pack()
 
-btn = Button(window, text = "Generate Report", command = create_report, font = ("Arial",20))
-btn.pack()
+btn1 = Button(window, text = "Generate Report", command = create_report, font = 30)
+btn1.pack()
+
+btn2 = Button(window, text = "View Statistics", command = view_statistics, font = 30)
+btn2.pack()
+
+btn3 = Button(window, text = "Full Feedback List", command = view_all, font = 30)
+btn3.pack()
 
 
 window.mainloop()
 
 
-#root.mainloop()
-        #if row > 0:
-            #text = (row[2])
-
-#            comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-            #text = "I am not sure about my feelings towards Calculus."
- #           result = json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4)
-  #          stuff = json.loads(result)
-
-           # print(stuff)
-
-    #        result = ""
-   #         for k in stuff.items():
-     #           if 'Sentiment' in k:
-      #              result = k[1].lower()
-       #             print(k[1])
-
-        #    for j in stuff.items():
-         #       if 'SentimentScore' in j:
-          #          for i in (j[1]).keys():
-           #             if result.lower() in i.lower():
-            #                percentage = j[1][i] * 100    
-             #               print("%.2f" % percentage + "%")
 
